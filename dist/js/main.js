be34011795f5ebaceac761f21b5c3e88ff4beeef
clalil <cliljander@gmail.com>
2019-12-04 21:16:43 +0100

    Add js function for toggle menu

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+const toggleMenu = () => {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+
+    showMenu = false;
+  }
+}
